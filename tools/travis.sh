2fe4b643fb73b45ed2e7708a1f7b82397b07335d
Jianyun <jianyunt@users.noreply.github.com>
2017-11-09 16:05:21 -0800

    Make Appveyor and TravisCI open for reviewing (#71)
    * Make Appveyor and TravisCI open for reviewing
    
    * changed powershell process name pwsh on core
    
    * Added nightly Appveryor run

--- tools/travis.sh
+++ tools/travis.sh
@@ -7,6 +7,6 @@ echo "TRAVIS_EVENT_TYPE value $TRAVIS_EVENT_TYPE"
 git submodule update --init
 
 
-powershell -c "cd src; ./bootstrap.ps1; ./build.ps1 -framework "netcoreapp2.0" Release"
+pwsh  -c "cd src; ./bootstrap.ps1; ./build.ps1 -framework "netcoreapp2.0" Release"
 
-sudo powershell -c "Import-Module ./tools/setup.psm1; Invoke-SHiPSTest"
+sudo pwsh  -c "Import-Module ./tools/setup.psm1; Invoke-SHiPSTest"
