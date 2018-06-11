68d8680bc5aa6b2914a015df652e0edf725c29a3
REDMOND\bingfli <bingfli@microsoft.com>
2018-06-11 16:07:47 -0700

    temp added nodeservicebase

---
+++ src/Microsoft.PowerShell.SHiPS/Node/NodeServiceBase.cs
@@ -0,0 +1,33 @@
+﻿using CodeOwls.PowerShell.Provider.PathNodes;
+
+namespace Microsoft.PowerShell.SHiPS
+{
+    /// <summary>
+    /// Defines actions that applies to a SHiPSLeaf node.
+    /// </summary>
+    internal class LeafNodeService : PathNodeBase
+    {
+        private readonly SHiPSLeaf _shipsLeaf;
+        private static readonly string _leaf = ".";
+
+        internal LeafNodeService(object leafObject)
+        {
+            _shipsLeaf = leafObject as SHiPSLeaf;
+        }
+
+        public override IPathValue GetNodeValue()
+        {
+            return new LeafPathValue(_shipsLeaf, Name);
+        }
+
+        public override string ItemMode
+        {
+            get {return _leaf; }
+        }
+
+        public override string Name
+        {
+            get { return _shipsLeaf.Name; }
+        }
+    }
+}
